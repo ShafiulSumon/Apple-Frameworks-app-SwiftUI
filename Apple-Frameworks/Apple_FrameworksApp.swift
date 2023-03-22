b2adf315295ef3a9ebc2552985c09468d33f8220
Md_Shafiul_Alam <shafiul.alam@bjitgroup.com>
2023-03-22 12:27:33 +0600

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by ShafiulAlam-00058 on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
